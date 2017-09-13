77d2f4b34ae6caafcc3ab2c70c6d7fe476d5629f
kitizl <nbalasub@ucsc.edu>
2017-09-13 21:29:54 +0530

    added name_scores

---
+++ name_scores/names.py
@@ -0,0 +1,29 @@
+#! python3
+
+import time
+
+start_time = time.time()
+
+def value_name(name):
+    mapper = {n:i+1 for (i,n) in enumerate(list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))}
+    return sum([mapper[x] for x in name])
+
+def sort_names(names):
+    # using quicksort
+    if names == []:
+        return []
+    pivot = names[0]
+    l = sort_names([x for x in names[1:] if x < pivot])
+    u = sort_names([x for x in names[1:] if x >= pivot])
+    return l + [pivot] + u
+
+# opening the file and converting it into a list
+name_handle = open("names.txt")
+name_list = [name[1:-1] for name in name_handle.read().split(",")]
+
+name_score = sum([i*value_name(x) for (i,x) in enumerate(sort_names(name_list))])
+
+end_time = time.time()
+
+print("Name score for the given file: %d" %name_score)
+print("Time taken for computation: %0.5f seconds." %(end_time-start_time))
